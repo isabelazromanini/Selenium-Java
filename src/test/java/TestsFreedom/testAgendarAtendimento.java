532b376308d8e6369565982265518d6660ccc66d
isabelazromanini <bella_zr@hotmail.com>
2018-09-18 15:55:31 -0300

    primeiro commit

--- src/test/java/TestsFreedom/testAgendarAtendimento.java
+++ src/test/java/TestsFreedom/testAgendarAtendimento.java
@@ -1,4 +1,13 @@
 package TestsFreedom;
 
+import org.testng.annotations.Test;
+
 public class testAgendarAtendimento {
+
+    @Test (description = "Teste Agendar Atendimento")
+
+    public void Test1(){
+
+    }
+
 }
